28b5007b3ec925694ba2c490fbea3ada5269b2d4
WindfuryBulb <mgr2009@msn.com>
2017-02-28 22:24:10 +0800

    add json output

---
+++ MapServer/mpserver/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h2>Hello World!</h2>A Server for a Feng Ru Cup Project.<br><br><footer><center>College of Software, Beihang University</center></footer>")
+
+def getmsg(request):
+    res={}
+    res['msg']="hehehe"
+    res['code']="python"
+    ans=json.dumps(res)
+    return HttpResponse(ans)
